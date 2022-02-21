503edae86349ab2988447b274c4631d82f3677a6
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2022-02-21 16:40:05 +0200

    mongodb

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require('mongoose');
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const {MONGODB_URL, PORT} = process.env;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+  app.listen(PORT, () => {
+  console.log("Database connection successful, Server running. Use our API on port: 3000")
+})
+})
+  .catch((error) => {
+    console.log(error.message)
+    process.exit(1)
 })
